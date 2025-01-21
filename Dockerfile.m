9a4f04607ac2224903cbce54565f01c6f58d5909
Paul Großmann <git@paulgrossmann.de>
2025-01-21 23:52:32 +0100

    fix second install cmd not working on arm

--- Dockerfile.m
+++ Dockerfile.m
@@ -4,10 +4,30 @@ FROM --platform=linux/amd64 ubuntu:22.04
 
 ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt update --fix-missing
-RUN apt install -y git gdb git wget patchelf file strace tmux python3 nasm binwalk bsdmainutils
-RUN apt install -y netcat python3-pip ruby-full valgrind vim xclip elfutils
-RUN apt install -y checksec
+RUN apt-get update --fix-missing && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    git \
+    gdb \
+    wget \
+    patchelf \
+    file \
+    strace \
+    tmux \
+    python3 \
+    nasm \
+    binwalk \
+    bsdmainutils \
+    netcat \
+    python3-pip \
+    ruby-full \
+    valgrind \
+    vim \
+    xclip \
+    elfutils \
+    checksec \
+    ca-certificates && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
 
 WORKDIR /opt
 RUN git clone https://github.com/pwndbg/pwndbg
@@ -16,8 +36,8 @@ ENV LC_ALL=C.UTF-8
 RUN ./setup.sh
 
 WORKDIR /opt
-RUN wget https://github.com/io12/pwninit/releases/download/3.3.0/pwninit
-RUN chmod +x pwninit
+RUN wget https://github.com/io12/pwninit/releases/download/3.3.0/pwninit && \
+    chmod +x pwninit
 
 RUN gem install one_gadget
 RUN pip3 install IPython icecream angr pwntools
